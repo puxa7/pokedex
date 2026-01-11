2a125ffa4eddf6070c2c0b3e44bf5f042bce7dc5
puxa7 <umpik123@gmail.com>
2026-01-11 11:14:54 +0100

    V0.1 wstepnie dziala

---
+++ dist/repl.js
@@ -0,0 +1,10 @@
+export function cleanInput(text) {
+    return text
+        .toLowerCase() // Zamiana na małe litery (np. "Pikachu" -> "pikachu")
+        .trim() // Usunięcie spacji z początku i końca
+        .split(/\s+/); // Rozbicie na tablicę według spacji (obsługuje też wiele spacji)
+}
+const input = "  Hello World  ";
+const cleaned = cleanInput(input);
+console.log(cleaned);
+// Powinno wypisać: ["hello", "world"]
---
+++ src/repl.test.ts
@@ -0,0 +1,56 @@
+import { cleanInput } from "./repl";
+import { describe, expect, test } from "vitest";
+
+// describe.each([
+//     {
+//         input: "  hello  world  ",
+//         expected: ["hello", "world"],
+//     },
+//     {
+//         input: "PIKACHU charizard",
+//         expected: ["pikachu", "charizard"],
+//     },
+//     {
+//         input: "  Bulbasaur  ",
+//         expected: ["bulbasaur"],
+//     }
+//     // TODO: more test cases here
+// ])("cleanInput($input)", ({ input, expected }) => {
+//     test(`Expected: ${expected}`, () => {
+//         // TODO: call cleanInput with the input here
+
+//         // The `expect` and `toHaveLength` functions are from vitest
+//         // they will fail the test if the condition is not met
+//         expect(actual).toHaveLength(expected.length);
+//         for (const i in expected) {
+//             // likewise, the `toBe` function will fail the test if the values are not equal
+//             expect(actual[i]).toBe(expected[i]);
+//         }
+//     });
+// });
+
+
+describe.each([
+  {
+    input: "   hello   world   ",
+    expected: ["hello", "world"],
+  },
+  {
+    input: "PIKACHU charizard",
+    expected: ["pikachu", "charizard"],
+  },
+  {
+    input: "  Bulbasaur  ",
+    expected: ["bulbasaur"],
+  }
+])("cleanInput($input)", ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    // TUTAJ MUSI BYĆ TA LINIA:
+    const actual = cleanInput(input); 
+
+    expect(actual).toHaveLength(expected.length);
+    for (const i in expected) {
+      expect(actual[i]).toBe(expected[i]);
+    }
+  });
+});
